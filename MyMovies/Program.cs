1e35b38862ab38c4413d4355405dc95355a75a23
Scott <tsgcats@gmail.com>
2025-06-26 14:16:28 -0400

    Removed args, from Main()

--- MyMovies/Program.cs
+++ MyMovies/Program.cs
@@ -1,2 +1,17 @@
-﻿/* MyMovies API (C) 2025 TSGxCode */
-Console.WriteLine("My Movies API! ");
+﻿using System;
+
+/* MyMovies API (C) 2025 TSGxCode */
+
+
+ class Program
+{
+    public static void Main()
+    {
+        Console.Write("Movie list");
+        _ = Console.ReadLine();
+
+
+
+        Console.WriteLine("My Movies API! ");
+    }
+}
